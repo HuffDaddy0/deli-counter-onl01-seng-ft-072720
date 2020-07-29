7a84fd2055f8fbf661563410c3c885dc85517246
Tyler Huffman <ty.huff24@gmail.com>
2020-07-29 17:15:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,11 @@ def line(array)
     new_array = []
     array.each_with_index do |name, index|
     new_array.push("#{index+1}: name")
+    puts "The line is currently: #{new_array.join(" ")}"
   end
   else
     puts "The line is currently empty."
   end
-  new_array.join(" ")
 end
 
 def take_a_number(array, name)
