489ffeb3da396778e119f795f84b5e7434ace407
Tyler Huffman <ty.huff24@gmail.com>
2020-07-29 17:12:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,14 @@
 # Write your code here.
 def line(array)
   if array.length >= 1
-    puts array.join(", ")
+    new_array = []
+    array.each_with_index do |name, index|
+    new_array.push("#{index+1}: name")
+  end
   else
     puts "The line is currently empty."
   end
+  new_array.join(" ")
 end
 
 def take_a_number(array, name)
