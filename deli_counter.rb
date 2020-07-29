ee874e2bede8136b893cf15d70219fd897eec942
Tyler Huffman <ty.huff24@gmail.com>
2020-07-29 16:57:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(array)
-  array.length >= 1 ? puts array : puts "the line is currently empty."
+  array.length >= 1 ? {puts array : puts "the line is currently empty."}
 end
 
 def take_a_number(array, name)
