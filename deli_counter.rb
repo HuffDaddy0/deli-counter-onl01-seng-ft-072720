bf5711c3e3fe2ef56bf54f2ac6897a20e0f64e63
Tyler Huffman <ty.huff24@gmail.com>
2020-07-29 17:16:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(array)
   if array.length >= 1
     new_array = []
     array.each_with_index do |name, index|
-    new_array.push("#{index+1}: name")
+    new_array.push("#{index+1}: #{name}")
     puts "The line is currently: #{new_array.join(" ")}"
   end
   else
